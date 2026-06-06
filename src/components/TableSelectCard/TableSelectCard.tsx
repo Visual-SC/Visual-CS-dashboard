fc3841902cf80d7acdb7b86b98c1c71997bfaf05
Juan David Franco <jfrancoh5@gmail.com>
2026-06-06 14:52:45 -0500

    feat: add console log for dayOptions in TableSelectCard component for debugging purposes

--- src/components/TableSelectCard/TableSelectCard.tsx
+++ src/components/TableSelectCard/TableSelectCard.tsx
@@ -3,6 +3,7 @@ import type { TableSelectCardProps } from './type';
 
 const TableSelectCard: React.FC<TableSelectCardProps> = ({ label }) => {
   const { dayOptions, isLoading, error } = useTableSelectCard();
+  console.log(dayOptions)
 
   return (
     <div className="flex flex-col">
